ed4255dd3fc9d33a810b23074ed9593e9f11232b
peach <108270895+kishart@users.noreply.github.com>
2023-09-14 10:49:35 +0800

    Add files via upload

---
+++ php-and-mysql-activity-main/dashboard.php
@@ -0,0 +1,19 @@
+<?php
+//include auth_session.php file on all user panel pages
+include("auth_session.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Dashboard - Client area</title>
+    <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+    <div class="pic">
+       
+        <img src="./hak.jpg" alt="bunny">
+        <button style="color:white;"><a href="logout.php">Logout</a></button>
+    </div>
+</body>
+</html>
